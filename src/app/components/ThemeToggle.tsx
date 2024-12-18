23ded605cacc70bba4486b4f1cc498de95ebc65a
WonYunSun <ereunosun113@gmail.com>
2024-12-19 02:23:47 +0900

    fix : ThemeToggle 컴포넌트 수정

--- src/app/components/ThemeToggle.tsx
+++ src/app/components/ThemeToggle.tsx
@@ -1,9 +1,21 @@
 "use client";
 
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 const ThemeToggle = () => {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    // 컴포넌트가 클라이언트에서만 렌더링되도록 설정
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    // 클라이언트에서만 렌더링하도록 대체
+    return null;
+  }
 
   return (
     <button
